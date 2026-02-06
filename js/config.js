c2de269445f84ac1247accb531569644e6ad2504
Diseven1990 <diogocruz7@gmail.com>
2026-02-06 10:34:33 +0000

    Change font size of grade field in config

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ window.CERT_CONFIG = {
      fields: {
   name:  { pageIndex: 0, x: 231.6, y: 306.3, w: 320, fontSize: 22.76 },
   cc:    { pageIndex: 0, x: 383.8, y: 283.4, w: 320, fontSize: 22.76 },
-  grade: { pageIndex: 0, x: 67.9, y: 132.4, w: 200, fontSize: 56 }
+  grade: { pageIndex: 0, x: 67.9, y: 132.4, w: 200, fontSize: 40 }
 },
       rules: { goldFrom: 80 } // >=80 ouro, <80 cinzento
     }
