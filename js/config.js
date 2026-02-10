a5ad8dca6a379e367a54976a6f87cb18e5857752
Diseven1990 <diogocruz7@gmail.com>
2026-02-10 14:46:29 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ window.CERT_CONFIG = {
    fields: {
   name:  { pageIndex: 0, x: 229.7, y: 306.8, w: 320, fontSize: 22.76 },
   cc:    { pageIndex: 0, x: 381.9, y: 277.2, w: 320, fontSize: 22.76 },
-  grade: { pageIndex: 0, x: 46.9, y: 123.3, w: 200, fontSize: 56 }
+  grade: { pageIndex: 0, x: 46.9, y: 123.3, w: 200, fontSize: 40 }
 },
       rules: { goldFrom: 80 } // >=80 ouro, <80 cinzento
     }
