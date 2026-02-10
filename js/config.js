9ed2c12853f9e7acf3e29f772184f889d2bf2d1d
Diseven1990 <diogocruz7@gmail.com>
2026-02-10 14:52:53 +0000

    Update position of name field in config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ window.CERT_CONFIG = {
         grey: "templates/photoshop_cin.pdf"
       },
    fields: {
-  name:  { pageIndex: 0, x: 229.7, y: 306.8, w: 320, fontSize: 22.76 },
+  name:  { pageIndex: 0, x: 955.5, y: 937.5, w: 320, fontSize: 22.76 },
   cc:    { pageIndex: 0, x: 381.9, y: 277.2, w: 320, fontSize: 22.76 },
   grade: { pageIndex: 0, x: 46.9, y: 123.3, w: 200, fontSize: 40 }
 },
