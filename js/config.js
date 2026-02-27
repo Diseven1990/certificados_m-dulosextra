a8a7f908d90843b982704a668b91e773773b1f60
Diseven1990 <diogocruz7@gmail.com>
2026-02-27 16:08:24 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,9 +8,9 @@ window.CERT_CONFIG = {
         grey: "templates/photoshop_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 227.3, y: 303.9, w: 320, fontSize: 22 },
+        cc:    { pageIndex: 0, x: 383.3, y: 277.2, w: 320, fontSize: 22 },
+        grade: { pageIndex: 0, x: 46.4, y: 123.3, w: 200, fontSize: 40 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [1, 1, 1] }
@@ -23,9 +23,9 @@ window.CERT_CONFIG = {
         grey: "templates/lightroom_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 228.3, y: 303.0, w: 320, fontSize: 22 },
+        cc:    { pageIndex: 0, x: 381.4, y: 274.3, w: 320, fontSize: 22 },
+        grade: { pageIndex: 0, x: 46.4, y: 119.5, w: 200, fontSize: 40 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [0, 0, 0] }
@@ -38,9 +38,9 @@ window.CERT_CONFIG = {
         grey: "templates/premiere_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 228.3, y: 303.0, w: 320, fontSize: 22 },
+        cc:    { pageIndex: 0, x: 383.3, y: 276.2, w: 320, fontSize: 22 },
+        grade: { pageIndex: 0, x: 45.5, y: 123.3, w: 200, fontSize: 40 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [1, 1, 1] }
@@ -53,9 +53,9 @@ window.CERT_CONFIG = {
         grey: "templates/swonkie_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 591.0, y: 295.3, w: 320, fontSize: 22 },
+        cc:    { pageIndex: 0, x: 486.7, y: 265.7, w: 320, fontSize: 22 },
+        grade: { pageIndex: 0, x: 448.4, y: 109.9, w: 200, fontSize: 40 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [1, 1, 1] }
@@ -68,9 +68,9 @@ window.CERT_CONFIG = {
         grey: "templates/closum_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 585.2, y: 296.3, w: 320, fontSize: 22 },
+        cc:    { pageIndex: 0, x: 485.7, y: 266.6, w: 320, fontSize: 22 },
+        grade: { pageIndex: 0, x: 450.3, y: 112.8, w: 200, fontSize: 40 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [1, 1, 1] }
@@ -83,9 +83,9 @@ window.CERT_CONFIG = {
         grey: "templates/canva_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 277.1, y: 351.7, w: 320, fontSize: 22 },
+        cc:    { pageIndex: 0, x: 460.8, y: 321.1, w: 320, fontSize: 22 },
+        grade: { pageIndex: 0, x: 82.8, y: 149.1, w: 200, fontSize: 50 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [1, 1, 1] }
@@ -98,9 +98,9 @@ window.CERT_CONFIG = {
         grey: "templates/chatgpt_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 286.6, y: 352.7, w: 320, fontSize: 30 },
+        cc:    { pageIndex: 0, x: 470.4, y: 319.2, w: 320, fontSize: 30 },
+        grade: { pageIndex: 0, x: 99.1, y: 132.8, w: 200, fontSize: 45 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [1, 1, 1] }
@@ -113,9 +113,9 @@ window.CERT_CONFIG = {
         grey: "templates/inshot_cin.pdf"
       },
       fields: {
-        name:  { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        cc:    { pageIndex: 0, x: 0, y: 0, w: 320, fontSize: 22 },
-        grade: { pageIndex: 0, x: 0, y: 0, w: 200, fontSize: 56 }
+        name:  { pageIndex: 0, x: 277.1, y: 351.7, w: 320, fontSize: 30 },
+        cc:    { pageIndex: 0, x: 459.9, y: 319.2, w: 320, fontSize: 30 },
+        grade: { pageIndex: 0, x: 85.7, y: 148.1, w: 200, fontSize: 40 }
       },
       rules: { goldFrom: 80 },
       style: { textColorRgb: [1, 1, 1] }
